14f4199506467f404f790433aca3af74fe8598c6
parisa fallahi <parisafallahi71@gmail.com>
2023-10-04 13:10:52 +0330

    header section done

--- swiper.js
+++ swiper.js
@@ -3,23 +3,24 @@ var swiper = new Swiper(".mySwiper", {
     delay: 2000,
   },
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 20,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
    
     breakpoints: {
+    
       640: {
         slidesPerView: 2,
         spaceBetween: 20,
       },
       768: {
-        slidesPerView: 4,
+        slidesPerView: 2,
         spaceBetween: 40,
       },
       1024: {
-        slidesPerView: 5,
+        slidesPerView: 3,
         spaceBetween: 50,
       },
     },
